4a3fba94b7027afd36f84fd370da649ff47f46a0
Menma69 <uttkarshmalviya90@gmail.com>
2025-03-26 10:27:50 +0530

    deployment changes

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,6 +1,9 @@
 import express from "express";
 import cors from "cors";
 import authRoute from "./routes/auth";
+import dotenv from "dotenv";
+dotenv.config();
+
 
 const app = express();
 app.use(express.json());
